2c5910953fe2b41dc2c51b6bdf3e8c23853ce173
Byakko-star <thatboii1019@outlook.com>
2024-01-21 22:04:10 -0600

    added category also fixed my misspelling on class greeting

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,31 @@
 import {ref} from 'vue'
 const todos = ref([])
 const name = ref('')
-const input_content = ref ('')
+const input_content = ref('')
+const input_category = ref(null)
 
 const addTodo = () => {
-  if(input_content.value.trim() === ''){
+  if(input_content.value.trim() === '' || input_category.value == null) {
     return
   }
 
   todos.value.push({
     content: input_content.value,
+    category: input_category.value,
   })
 
   input_content.value = ''
+  input_category.value = null
+
   //console.log(todos)
 
 }
+
 </script>
 
 <template>
   <main class="app">
-    <section class="greatins">
+    <section class="greeting">
       <h2 class="title">
         What's up, <input type="text" placeholder="name" v-model="name" />
         <!-- {{ name }} -->
@@ -34,6 +39,23 @@ const addTodo = () => {
         <h4>Whats on your to do list?</h4>
         <input type="text" placeholder="e.g, Make a video" v-model="input_content" />
         <!-- {{ input_content }} -->
+
+        <h4>Pick a Category</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" value="business" v-model="input_category" />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" value="perosnal" v-model="input_category" />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+
+          <!-- {{ input_category }} -->
+        </div>
       </form>
     </section>
 
